77e13f90c1000377733ab6fb57823ee236b9c2d5
Hậu Nguyễn <82701879+Vitkonlontonn@users.noreply.github.com>
2023-03-22 14:03:34 +0700

    fix db & template
    thêm is_pinned column vào table posts. Giao diện cho admin & login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //Language::factory(10)->create();
-        //Company::factory(10)->create();
+        Language::factory(10)->create();
+        Company::factory(10)->create();
         //User::factory(10)->create();
     $this -> call(UserSeeder::class);
     }
---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,95 @@
+<div class="left-side-menu mm-show">
+
+    <!-- LOGO -->
+    <a href="index.html" class="logo text-center logo-light">
+                    <span class="logo-lg">
+                        <img src="assets/images/logo.png" alt="" height="16">
+                    </span>
+        <span class="logo-sm">
+                        <img src="assets/images/logo_sm.png" alt="" height="16">
+                    </span>
+    </a>
+
+    <!-- LOGO -->
+    <a href="index.html" class="logo text-center logo-dark">
+                    <span class="logo-lg">
+                        <img src="assets/images/logo-dark.png" alt="" height="16">
+                    </span>
+        <span class="logo-sm">
+                        <img src="assets/images/logo_sm_dark.png" alt="" height="16">
+                    </span>
+    </a>
+
+    <div class="h-100 mm-active" id="left-side-menu-container" data-simplebar="init">
+        <div class="simplebar-wrapper" style="margin: 0px;">
+            <div class="simplebar-height-auto-observer-wrapper">
+                <div class="simplebar-height-auto-observer"></div>
+            </div>
+            <div class="simplebar-mask">
+                <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
+                    <div class="simplebar-content-wrapper" style="height: 100%; overflow: hidden scroll;">
+                        <div class="simplebar-content" style="padding: 0px;">
+
+                            <!--- Sidemenu -->
+                            <ul class="metismenu side-nav mm-show">
+
+                                <li class="side-nav-title side-nav-item">Navigation</li>
+
+                                <li class="side-nav-item">
+                                    <a href="javascript: void(0);" class="side-nav-link">
+                                        <i class="uil-home-alt"></i>
+                                        <span class="badge badge-success float-right">4</span>
+                                        <span> Dashboards </span>
+                                    </a>
+                                    <ul class="side-nav-second-level mm-collapse" aria-expanded="false">
+                                        <li>
+                                            <a href="dashboard-analytics.html">Analytics</a>
+                                        </li>
+                                        <li>
+                                            <a href="dashboard-crm.html">CRM</a>
+                                        </li>
+                                        <li>
+                                            <a href="index.html">Ecommerce</a>
+                                        </li>
+                                        <li>
+                                            <a href="dashboard-projects.html">Projects</a>
+                                        </li>
+                                    </ul>
+                                </li>
+
+
+                            </ul>
+
+                            <!-- Help Box -->
+                            <div class="help-box text-white text-center">
+                                <a href="javascript: void(0);" class="float-right close-btn text-white">
+                                    <i class="mdi mdi-close"></i>
+                                </a>
+                                <img src="assets/images/help-icon.svg" height="90" alt="Helper Icon Image">
+                                <h5 class="mt-3">Unlimited Access</h5>
+                                <p class="mb-3">Upgrade to plan to get access to unlimited reports</p>
+                                <a href="javascript: void(0);" class="btn btn-outline-light btn-sm">Upgrade</a>
+                            </div>
+                            <!-- end Help Box -->
+                            <!-- End Sidebar -->
+
+                            <div class="clearfix"></div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="simplebar-placeholder" style="width: auto; height: 452px;"></div>
+        </div>
+        <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;">
+            <div class="simplebar-scrollbar"
+                 style="width: 0px; display: none; transform: translate3d(0px, 0px, 0px);"></div>
+        </div>
+        <div class="simplebar-track simplebar-vertical" style="visibility: visible;">
+            <div class="simplebar-scrollbar"
+                 style="height: 299px; transform: translate3d(0px, 0px, 0px); display: block;"></div>
+        </div>
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
